345aff7bad0ce112f97b5244cfba650f7848316a
ana446 <70743735+ana446@users.noreply.github.com>
2021-10-27 07:46:24 -0500

    add filter function for buttons in addorders view

--- src/app/views/add-orders/add-orders.component.ts
+++ src/app/views/add-orders/add-orders.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { faPlusCircle, faMinusCircle, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
-
+import { IProductDetail } from 'src/app/models/product-model';
+import { ProductService } from 'src/app/services/product/product.service';
 @Component({
   selector: 'app-add-orders',
   templateUrl: './add-orders.component.html',
@@ -8,9 +9,43 @@ import { faPlusCircle, faMinusCircle, faTrashAlt } from '@fortawesome/free-solid
 })
 export class AddOrdersComponent implements OnInit {
 
-  constructor() { }
+  products: Array<IProductDetail>;
+  product!: Object;
+  productsType = new Set();
+  typesProduct = 'bebidas';
+  items: Array<IProductDetail>;
+  show: boolean;
+  showButton: boolean;
+
+  constructor(private productService: ProductService) {
+    this.products = []
+    this.items = []
+    this.show = false;
+    this.showButton = false
+
+  }
 
   ngOnInit(): void {
+    this.showProducts();
+  }
+
+  showProducts() {
+    this.productService.getProducts().subscribe(
+      (response: any) => {
+        console.log(response);
+        this.allProducts(response);
+        this.filterType('Bebidas')
+      }
+    )
+  }
+  allProducts(product: Array<IProductDetail>) {
+    this.products = product;
+  }
+
+  filterType(category: any) {
+    this.items = this.products.filter((elem: IProductDetail) => {
+      return elem.type === category;
+    })
   }
   faPlusCircle = faPlusCircle;
   faMinusCircle = faMinusCircle;
